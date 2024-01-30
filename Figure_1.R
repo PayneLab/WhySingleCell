507967988263dc665d7eb1e6aed45c50ca66fcee
nitz1bug <nitz1bug@byu.edu>
2024-01-30 10:46:29 -0700

    add code for figures

---
+++ Figure_1.R
@@ -0,0 +1,129 @@
+library(tidyverse)
+library(ragg)
+library(viridis)
+
+
+
+## Read in the data
+### (1) list of TS proteins
+### (2) list of TE proteins
+### (3) protein information table from Hannah and Caleb and add TS/TE info
+TS_ids <- read_tsv("ensembl_to_uniprot_TS_NEW.tsv") %>%
+  filter(Reviewed == "reviewed") %>%
+  filter(Organism == "Homo sapiens (Human)") %>%
+  pull(Entry) %>%
+  unique() %>%
+  sort() %>%
+  unlist()
+
+TE_ids <- read_tsv("ensembl_to_uniprot_TE_NEW.tsv") %>%
+  filter(Reviewed == "reviewed") %>%
+  filter(Organism == "Homo sapiens (Human)") %>%
+  pull(Entry) %>%
+  unique() %>%
+  sort() %>%
+  unlist()
+
+proteins <- read_tsv("proteins.tsv") %>%
+  mutate(present_sc_new = select(., new_single_cell_F1:new_single_cell_F19) %>% rowSums(na.rm = TRUE)) %>%
+  mutate(present_sc_old = select(., old_single_cell_F3:old_single_cell_F5) %>% rowSums(na.rm = TRUE)) %>%
+  mutate(logscale = log10(bulk_abundance)) %>%
+  mutate(tissue_specific = uniprot_id %in% TS_ids) %>%
+  mutate(tissue_enriched = uniprot_id %in% TE_ids)
+
+
+## Create bins for the x-axis of the plot
+bins <- seq(1.1, 7.9, 0.1)
+labels <- bins[-69] %>% paste()
+
+
+## Create filtered data frames
+new_sc = proteins %>%
+  filter(present_sc_new > 0) %>%
+  mutate(bins = factor(cut(logscale, breaks = bins, labels = labels), levels = labels))
+
+old_sc <- proteins %>%
+  filter(present_sc_old > 0) %>%
+  mutate(bins = factor(cut(logscale, breaks = bins, labels = labels), levels = labels))
+
+bulk <- proteins %>%
+  filter(!is.na(bulk_abundance)) %>%
+  mutate(bins = factor(cut(logscale, breaks = bins, labels = labels), levels = labels))
+
+TS <- proteins %>%
+  filter(tissue_specific == TRUE) %>%
+  mutate(bins = factor(cut(logscale, breaks = bins, labels = labels), levels = labels))
+
+TE <- proteins %>%
+  filter(tissue_enriched == TRUE) %>%
+  mutate(bins = factor(cut(logscale, breaks = bins, labels = labels), levels = labels))
+
+
+## Value counts
+new_sc_counts <- new_sc %>%
+  group_by(bins, .drop = FALSE) %>%
+  summarize(Count_New = n()) %>%
+  arrange(desc(Count_New)) %>%
+  filter(!is.na(bins))
+
+old_sc_counts <- old_sc %>%
+  group_by(bins, .drop = FALSE) %>%
+  summarize(Count_Old = n()) %>%
+  arrange(desc(Count_Old)) %>%
+  filter(!is.na(bins))
+
+bulk_counts <- bulk %>%
+  group_by(bins, .drop = FALSE) %>%
+  summarize(Count_Bulk = n()) %>%
+  arrange(desc(Count_Bulk)) %>%
+  filter(!is.na(bins))
+
+TS_counts <- TS %>% 
+  group_by(bins, .drop = FALSE) %>%
+  summarize(Count_TS = n()) %>%
+  arrange(desc(Count_TS)) %>%
+  filter(!is.na(bins))
+
+TE_counts <- TE %>% 
+  group_by(bins, .drop = FALSE) %>%
+  summarize(Count_TE = n()) %>%
+  arrange(desc(Count_TE)) %>%
+  filter(!is.na(bins))
+
+
+## Combine counts tables into two: counts for the line distributions and 
+##  counts for the features in the bar chart
+combined_distributions <- new_sc_counts %>%
+  full_join(old_sc_counts) %>%
+  full_join(bulk_counts) %>%
+  mutate(bins = as.numeric(as.character(bins))) %>%
+  arrange(bins) %>%
+  pivot_longer(Count_New:Count_Bulk, names_to = "dataset", values_to = "counts")
+
+combined_features <- TS_counts %>%
+  full_join(TE_counts) %>%
+  mutate(bins = as.numeric(as.character(bins))) %>%
+  arrange(bins) %>%
+  mutate(fill = "filler")
+
+
+## Create a line+bar plot
+ggplot() +
+  geom_bar(combined_features, mapping = aes(x = bins, y = Count_TE, fill = "Tissue-Enriched"), stat = "identity") +
+  geom_bar(combined_features, mapping = aes(x = bins, y = Count_TS, fill = "Tissue-Specific"), stat = "identity") +
+  geom_line(combined_distributions, mapping = aes(x = bins, y = counts, color = dataset), size = 1) +
+  theme_bw() +
+  labs(x = "Copy Number (log10)", y = "Number of Proteins", color = "Datasets", fill = "Featuresets") +
+  scale_x_continuous(breaks = seq(1, 8, 1)) +
+  scale_y_continuous(breaks = seq(0, 500, 100)) +
+  scale_fill_manual(name = "Featuresets", breaks = c("Tissue-Enriched", "Tissue-Specific"), values=c("Tissue-Enriched" = "gray45", "Tissue-Specific" = "black")) +
+  scale_color_viridis(discrete = TRUE, labels = c("Bulk Proteomics", "SCP - 2023", "SCP - 2021")) + 
+  guides(linewidth = "none",
+         fill = guide_legend(order = 2),
+         color = guide_legend(order = 1))
+
+
+## Save the plot
+ggsave("Depth Classification Distributions.png", dpi = 600, units = "in", height = 5, width = 7)
+
+
---
+++ Figure_2ab.R
@@ -0,0 +1,130 @@
+library(tidyverse)
+library(viridis)
+library(ragg)
+
+
+## Set seed for replication
+set.seed(77)
+
+#### PART A ####
+## Generate random clusters in a bifurcation structure
+points_per_cluster <- 50
+
+### Step 1: generate cluster 1 (it will be the left-most cluster, centered on the y-axis)
+x_values <- rnorm(points_per_cluster, mean = 10, sd = 2)
+y_values <- rnorm(points_per_cluster, mean = 15, sd = 2)
+
+cluster_1 <- tibble(x_values, y_values) %>%
+  mutate(cluster = "Cluster 1")
+
+### Step 2: generate cluster 2 (it will be to the right of cluster 1, centered on the y-axis)
+x_values <- rnorm(points_per_cluster, mean = 16, sd = 2)
+y_values <- rnorm(points_per_cluster, mean = 15, sd = 2)
+
+cluster_2 <- tibble(x_values, y_values) %>%
+  mutate(cluster = "Cluster 2")
+
+### Step 3: generate cluster 3 (it will be to the right of cluster 2, on the top end of the y-axis)
+x_values <- rnorm(points_per_cluster, mean = 22, sd = 2)
+y_values <- rnorm(points_per_cluster, mean = 20, sd = 2)
+
+cluster_3 <- tibble(x_values, y_values) %>%
+  mutate(cluster = "Cluster 3")
+
+### Step 4: generate cluster 4 (it will be to the right of cluster 2, on the bottom end of the y-axis)
+x_values <- rnorm(points_per_cluster, mean = 22, sd = 2)
+y_values <- rnorm(points_per_cluster, mean = 10, sd = 2)
+
+cluster_4 <- tibble(x_values, y_values) %>%
+  mutate(cluster = "Cluster 4")
+
+### Step 5: combine the data points into one table
+clusters <- bind_rows(cluster_1, cluster_2, cluster_3, cluster_4)
+
+### Step 6: create data points for a line plot to represent a trajectory 
+###   overlayed on top of the scatterplot
+trajectory_points <- clusters %>%
+  group_by(cluster) %>%
+  summarize(x_values = mean(x_values), y_values = mean(y_values))
+
+trajectory_line1 <- trajectory_points %>%
+  filter(cluster != "Cluster 4")
+
+trajectory_line2 <- trajectory_points %>%
+  filter(cluster == "Cluster 2" | cluster == "Cluster 4")
+
+### Step 7: plot the clusters
+trajectory_plot <- ggplot() + 
+  geom_point(clusters, mapping = aes(x = x_values, y = y_values, color = cluster)) +
+  scale_color_viridis(discrete = TRUE) +
+  # geom_point(clusters, mapping = aes(x = x_values, y = y_values, color = "red")) + 
+  geom_point(trajectory_points, mapping = aes(x_values, y_values), color = "black", size = 2.5) + 
+  geom_line(trajectory_line1, mapping = aes(x_values, y_values), color = "black") + 
+  geom_line(trajectory_line2, mapping = aes(x_values, y_values), color = "black") +
+  theme_bw() + 
+  labs(x = "X", y = "Y", title = "a)") + 
+  theme(legend.position = "none",
+        axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  # theme(legend.position = "none", 
+  #       axis.text.x = element_blank(), axis.ticks.x = element_blank(), 
+  #       axis.text.y = element_blank(), axis.ticks.y = element_blank())
+
+
+
+#### PART B ####
+## Split full dataset above into replicates
+### Step 1: Randomly assigne a replicate number to each row in the clusters dataframe
+replicates <- sample(rep(1:4, each = points_per_cluster))
+
+clusters <- clusters %>%
+  mutate(replicate = replicates)
+
+### Step 2: create data points for a line plot to represent a trajectory 
+###   overlayed on top of the scatterplot
+trajectory_points <- clusters %>%
+  group_by(replicate, cluster) %>%
+  summarize(x_values = mean(x_values), y_values = mean(y_values))
+
+trajectory_line1 <- trajectory_points %>%
+  filter(ifelse(replicate != 2, 
+                cluster != "Cluster 4", 
+                cluster != "Cluster 3"))
+
+trajectory_line2 <- trajectory_points %>%
+  filter(ifelse(replicate != 2, 
+                cluster == "Cluster 2" | cluster == "Cluster 4", 
+                cluster == "Cluster 3" | cluster == "Cluster 4"))
+
+### Spet 3: Plot the replicate trajectories
+replicate_plot <- ggplot() + 
+  geom_point(clusters, mapping = aes(x = x_values, y = y_values, color = cluster)) + 
+  scale_color_viridis(discrete = TRUE) +
+  geom_point(trajectory_points, mapping = aes(x_values, y_values), color = "black", size = 2.5) +
+  geom_line(trajectory_line1, mapping = aes(x_values, y_values), color = "black") +
+  geom_line(trajectory_line2, mapping = aes(x_values, y_values), color = "black") +
+  facet_wrap(~replicate) + 
+  theme_bw() + 
+  labs(x = "X", y = "Y", title = "b)") + 
+  theme(legend.position = "none",
+        axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  # theme(legend.position = "none", 
+  #       axis.text.x = element_blank(), axis.ticks.x = element_blank(), 
+  #       axis.text.y = element_blank(), axis.ticks.y = element_blank())
+
+
+
+
+## Save the plots
+agg_png("Trajectory_No_Reps.png", width = 6, height = 6, units = "in", res = 600, scaling = 1.5)
+trajectory_plot
+dev.off()
+
+agg_png("Trajectory_Reps.png", width = 6, height = 6, units = "in", res = 600, scaling = 1.5)
+replicate_plot
+dev.off()
+
+
